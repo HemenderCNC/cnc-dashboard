482b20d609df49952cf5afcd9733d61464847931
codeandcore <marketing@agorareal.com>
2025-02-25 19:54:17 +0530

    clients and projects curd api done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Option;
-use App\Models\Skill;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Services\FileUploadService;
