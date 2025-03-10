a5301791c11107845f84210f477ee9684c18c492
HemenderCNC <hemender.cnc@gmail.com>
2025-03-10 14:55:00 +0530

    Activity Logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,13 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Services\GeneralSettingsService;
 
+use App\Models\ActivityLog;
+use App\Observers\ActivityLogObserver;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -27,5 +34,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $modelPath = app_path('Models');
+
+        if (File::isDirectory($modelPath)) {
+            $modelFiles = File::files($modelPath);
+            
+            foreach ($modelFiles as $file) {
+                $modelClass = 'App\\Models\\' . pathinfo($file->getFilename(), PATHINFO_FILENAME);
+                
+                if (class_exists($modelClass) && is_subclass_of($modelClass, Model::class)) {
+                    $modelClass::observe(ActivityLogObserver::class);
+                }
+            }
+        }
     }
 }
